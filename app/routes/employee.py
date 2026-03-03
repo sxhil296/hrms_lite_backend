cc6d2975aaa79ce7924cfb443d1455d3ae52b6ed
sahil <leviack629@gmail.com>
2026-03-04 04:35:31 +0530

    update get employee api

--- app/routes/employee.py
+++ app/routes/employee.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Query
 from app.database import employee_collection
 from app.schemas.employee import EmployeeCreate
 
@@ -8,23 +8,17 @@ router = APIRouter(prefix="/employees", tags=["Employees"])
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_employee(employee: EmployeeCreate):
 
-   existing = await employee_collection.find_one({
-    "$or": [
-        {"employee_id": employee.employee_id},
-        {"email": employee.email}
-    ]
-})
+    existing = await employee_collection.find_one({
+        "$or": [
+            {"employee_id": employee.employee_id},
+            {"email": employee.email}
+        ]
+    })
 
-if existing:
-    if existing["employee_id"] == employee.employee_id:
+    if existing:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
-            detail="Employee ID already exists"
-        )
-    if existing["email"] == employee.email:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail="Email already exists"
+            detail="Employee with same ID or email already exists"
         )
 
     result = await employee_collection.insert_one(employee.dict())
@@ -34,17 +28,50 @@ if existing:
         "id": str(result.inserted_id)
     }
 
-
 @router.get("/")
-async def get_employees():
+async def get_employees(
+    page: int = Query(1, ge=1),
+    limit: int = Query(5, ge=1, le=100),
+    search: str = Query("", min_length=0)
+):
+    skip = (page - 1) * limit
+
+    query = {}
+
+    if search:
+        query = {
+            "$or": [
+                {"full_name": {"$regex": search, "$options": "i"}},
+                {"email": {"$regex": search, "$options": "i"}},
+                {"employee_id": {"$regex": search, "$options": "i"}},
+            ]
+        }
+
+    total = await employee_collection.count_documents(query)
+
+    cursor = (
+        employee_collection
+        .find(query)
+        .skip(skip)
+        .limit(limit)
+        .sort("created_at", -1)
+    )
+
     employees = []
 
-    async for emp in employee_collection.find():
+    async for emp in cursor:
         emp["id"] = str(emp["_id"])
         del emp["_id"]
         employees.append(emp)
 
-    return employees
+    return {
+        "data": employees,
+        "total": total,
+        "page": page,
+        "limit": limit,
+        "hasNext": (total > page * limit),
+        "totalPages": (total + limit - 1) // limit
+    }
 
 
 @router.delete("/{employee_id}")
